44de9d8afbde1899d8817424ada33a48c0b8c136
VitaliiZaluskii <vzaluskiy23@gmail.com>
2024-03-12 22:01:16 +0200

    2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-const burgerBtn = document.querySelector(".burger__line");
+const burgerBtn = document.querySelector(".burger__lines");
 
 burgerBtn.addEventListener("click", function () {
-  document.querySelector(".burger__body").classList.toggle("hidden");
-  document.querySelector(".header__logo").classList.toggle("hidden");
-  document.querySelector(".header__logo-light").classList.toggle("hidden");
+  document.querySelector(".burger__line-two").classList.toggle("active");
+  document.querySelector(".burger__line-one").classList.toggle("active");
+  document.querySelector(".burger__line-three").classList.toggle("active");
+  document.querySelector(".burger-body").classList.toggle("hidden");
+  document.querySelector(".header__container").classList.toggle("active");
+  if (
+    document.querySelector(".header__logo").src.endsWith("Travelulu_logo.svg")
+  ) {
+    document.querySelector(".header__logo").src =
+      "img/Travelulu_logo(light).svg";
+  } else {
+    document.querySelector(".header__logo").src = "img/Travelulu_logo.svg";
+  }
+  if (
+    (document.querySelector(".burger__line-one").style.background =
+      "rgb(95, 46, 234);") &&
+    (document.querySelector(".burger__line-two").style.background =
+      "rgb(95, 46, 234);") &&
+    (document.querySelector(".burger__line-three").style.background =
+      "rgb(95, 46, 234);")
+  ) {
+    document.querySelector(".burger__line-one").style.backgroundColor =
+      "white;";
+    document.querySelector(".burger__line-two").style.backgroundColor =
+      "white;";
+    document.querySelector(".burger__line-three").style.backgroundColor =
+      "white;";
+  } else {
+    document.querySelector(".burger__line-one").style.background =
+      "rgb(95, 46, 234);";
+    document.querySelector(".burger__line-two").style.background =
+      "rgb(95, 46, 234);";
+    document.querySelector(".burger__line-three").style.background =
+      "rgb(95, 46, 234);";
+  }
 });
